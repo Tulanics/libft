c5607412d661e2e19aff0084853428f399e0571c
Tulanics <tulanics@gmail.com>
2021-09-19 21:24:34 -0300

    ft_substr.c ft_strjoin.c ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,39 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tconceic <tconceic@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/19 18:08:03 by tconceic          #+#    #+#             */
-/*   Updated: 2021/09/19 21:01:56 by tconceic         ###   ########.fr       */
+/*   Created: 2021/09/19 20:04:21 by tconceic          #+#    #+#             */
+/*   Updated: 2021/09/19 21:23:12 by tconceic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char			*str;
+	unsigned int	s1_len;
+	unsigned int	s2_len;
+	char			*str_end;
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	slen;
 
-	str = (char *)malloc(len + 1);
-	if (!malloc(len + 1))
+	if (!s1 && !s2)
 		return (NULL);
-	slen = ft_strlen(s);
-	i = start;
-	j = 0;
-	if (start < slen)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str_end = (char *)malloc(s1_len + s2_len + 1);
+	if (!(char *)malloc(s1_len + s2_len + 1))
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
 	{
-		while (i < start + len && s[i] != '\0')
-		{
-			str[j] = s[i];
-			j++;
-			i++;
-		}
+		str_end[i] = s1[i];
+		i++;
 	}
-	str[j] = '\0';
-	return (str);
+	j = 0;
+	while (j < s2_len)
+		str_end[i++] = s2[j++];
+	str_end[i] = '\0';
+	return (str_end);
 }
