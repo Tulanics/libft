f1122d3356874fdf04a52aae3605365ac2cafe09
Tulanics <tulanics@gmail.com>
2021-09-30 17:42:23 -0300

    ...

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tconceic <tconceic@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 20:57:39 by tconceic          #+#    #+#             */
-/*   Updated: 2021/09/30 17:38:54 by tconceic         ###   ########.fr       */
+/*   Updated: 2021/09/30 17:42:15 by tconceic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t count)
 
 	sc = (void *)src;
 	dt = (void *)dest;
+	if( *dt != NULL && *sc != NULL)
+		return (NULL);
 	i = 0;
-	while (i < count && *dt != NULL && *sc != NULL)
+	while (i < count &&)
 	{
 		dt[i] = sc[i];
 		i++;
